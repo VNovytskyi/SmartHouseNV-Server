09818614f6ee35bc21426f3f6937ae09ca505ab8
VNovytskyi <vladyslavnovytskyi2000@gmail.com>
2020-02-15 20:21:25 +0200

    Add regular expression for searching path and variables from request

--- Code/script1.js
+++ Code/script1.js
@@ -1,22 +1,52 @@
-console.log("Start");
-
 var wifi = require("Wifi");
 var http = require("http");
+
+var counter = 0;
 
 wifi.connect("MERCUSYS_7EBA", {password:"3105vlad3010vlada"}, function(err){
   if(err != null){
     console.log("Wifi connect error: " + err);
-  } 
+  }
   else{
-    console.log(wifi.getIP());
-    console.log(wifi.getStatus());
-    
+    console.log("WiFi connected. IP: " + wifi.getIP().ip);
+
     http.createServer(function(req, res){
-      console.log(req);
-      res.writeHead(200);
-      res.end("Hello world");
+      console.log(++counter + " " + req.url);
+
+      var re = /([a-z0-9]+=[a-z0-9]+)/g;
+      var found = req.url.match(re);
+
+      //re = /.+(?=\?)/; //Не поддерживается в этой версии
+      re = /.+\?/;
+      var request = req.url.match(re);
+      request = request[0].substring(0, request[0].length - 1);
+
+      console.log("Request: " + request);
+      console.log("Found: " + found);
+
+      switch(req.url){
+        case "/":
+        case "/home":
+          res.writeHead(200,{'Content-Type': 'text/html'});
+          res.end("Home Page");
+          break;
+
+        case "/settings":
+          res.writeHead(200,{'Content-Type': 'text/html'});
+          res.end("Settings Page");
+          break;
+
+        case "/set":
+          res.writeHead(200,{'Content-Type': 'text/html'});
+          res.end("set");
+          break;
+
+        default:
+          res.writeHead(404,{'Content-Type': 'text/html'});
+          res.end("404");
+          break;
+      }
+
     }).listen(80);
   }
 });
-
-console.log("End");
