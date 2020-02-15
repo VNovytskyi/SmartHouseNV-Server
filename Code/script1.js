6e8537a0a8c19a6a6f0ef4d831ce368d4d369dbc
VNovytskyi <vladyslavnovytskyi2000@gmail.com>
2020-02-15 22:29:44 +0200

    Add handler requests
    Add buildInLed

--- Code/script1.js
+++ Code/script1.js
@@ -1,7 +1,13 @@
 var wifi = require("Wifi");
 var http = require("http");
 
+const buildInLed = D2;
+
 var counter = 0;
+
+setInterval(function() {
+   buildInLed.toggle();
+}, 1000);
 
 wifi.connect("MERCUSYS_7EBA", {password:"3105vlad3010vlada"}, function(err){
   if(err != null){
@@ -11,23 +17,26 @@ wifi.connect("MERCUSYS_7EBA", {password:"3105vlad3010vlada"}, function(err){
     console.log("WiFi connected. IP: " + wifi.getIP().ip);
 
     http.createServer(function(req, res){
-      console.log(++counter + " " + req.url);
+      console.log(" ");
+      console.log("################## " + ++counter + " ##################");
 
-      var re = /([a-z0-9]+=[a-z0-9]+)/g;
-      var found = req.url.match(re);
+      var request = req.url.match(/.+\?/); //re = /.+(?=\?)/; //Не поддерживается в этой версии
 
-      //re = /.+(?=\?)/; //Не поддерживается в этой версии
-      re = /.+\?/;
-      var request = req.url.match(re);
-      request = request[0].substring(0, request[0].length - 1);
+      if(request){
+        request = request[0].substring(0, request[0].length - 1);
+
+        if(request[request.length-1] == '/')
+          request = request.substring(0, request.length - 1);
+      }else{
+        request = req.url;
+      }
 
       console.log("Request: " + request);
-      console.log("Found: " + found);
 
-      switch(req.url){
+      switch(request){
         case "/":
         case "/home":
-          res.writeHead(200,{'Content-Type': 'text/html'});
+          res.writeHead(200,{'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*'});
           res.end("Home Page");
           break;
 
@@ -37,12 +46,24 @@ wifi.connect("MERCUSYS_7EBA", {password:"3105vlad3010vlada"}, function(err){
           break;
 
         case "/set":
-          res.writeHead(200,{'Content-Type': 'text/html'});
-          res.end("set");
+          res.writeHead(200,{'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*'});
+
+          var commands = req.url.match(/([a-z0-9]+=[a-z0-9]+)/g);
+
+          if(commands.length){
+            //commands[0] - string
+            //
+            //TODO: Обеспечить выполнение команд из строки
+            console.log("Commands: " + commands);
+            res.end("Command: " + commands);
+          }else{
+            console.log("Empty command");
+            res.end("Empty command");
+          }
           break;
 
         default:
-          res.writeHead(404,{'Content-Type': 'text/html'});
+          res.writeHead(404);
           res.end("404");
           break;
       }
