05ae68083ecea177e003566fa67715aeb7bb1e54
VNovytskyi <vladyslavnovytskyi2000@gmail.com>
2020-02-21 00:21:31 +0200

    Update script1.js

--- ServerFirmware/script1.js
+++ ServerFirmware/script1.js
@@ -3,59 +3,82 @@ const storage = require("Storage");
 const WebSocket = require("ws");
 
 const buildInLed = D2;
+
+const WIFI_NAME = "MERCUSYS_7EBA";
+const WIFI_OPTIONS = {
+  password: "3105vlad3010vlada"
+};
+
 var counter = 0;
 var clients = [];
 
+function wsHandler(ws)
+{
+  clients.push(ws);
 
-wifi.connect("MERCUSYS_7EBA", {password:"3105vlad3010vlada"}, function(err){
-  if(err != null){
-    console.log("Wifi connect error: " + err);
-  }else {
-    console.log("http://" + wifi.getIP().ip);
+  ws.on('message', msg => {
+    //TODO: switch по командам
+    /*case "/set":
+      res.writeHead(200,{'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*'});
 
-    WebSocket.createServer(function(req, res){
-      console.log(" ");
-      console.log("################## " + ++counter + " ##################");
+      if(urlObj.query)
+        for(var key in urlObj.query){
+          console.log(key + " " + urlObj.query[key]);
 
-      var urlObj = url.parse(req.url, true);
+          switch(key)
+          {
+            case "buildInLed":
+              digitalWrite(buildInLed, urlObj.query[key] == "on" ? 0:1);
+              break;
+          }
+        }
 
-      switch(urlObj.pathname){
-        case "/":
-        case "/home":
-          res.writeHead(200,{'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*'});
-          res.end(storage.read("MainPage"));
-          break;
+      res.end("set");
+      break;*/
+    digitalWrite(D2, msg == 'on');
+    broadcast(msg);
+  });
 
-        case "/settings":
-          res.writeHead(200,{'Content-Type': 'text/html'});
-          res.end("Settings Page");
-          break;
+  ws.on('close', evt => {
+    var x = clients.indexOf(ws);
+    if (x > -1) {
+      clients.splice(x, 1);
+    }
+  });
+}
 
-        case "/set":
-          res.writeHead(200,{'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*'});
+function serverHandler(req, res)
+{
+  console.log(" ");
+  console.log("################## " + ++counter + " ##################");
 
-          if(urlObj.query)
-            for(var key in urlObj.query){
-              console.log(key + " " + urlObj.query[key]);
+  res.writeHead(200, {'Content-Type': 'text/html'});
 
-              switch(key)
-              {
-                case "buildInLed":
-                  digitalWrite(buildInLed, urlObj.query[key] == "on" ? 0:1);
-                  break;
-              }
-            }
+  const urlObj = url.parse(req.url, true);
+  console.log(urlObj.pathname);
 
-          res.end("set");
-          break;
+  switch(urlObj.pathname)
+  {
+    case "/":
+    case "/home":
+      res.writeHead(200,{'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*'});
+      res.end(storage.read("MainPage"));
+      break;
 
-        case "/socket":
-            res.writeHead(200,{'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*'});
-            res.end(`<html>
+    case "/settings":
+      res.writeHead(200,{'Content-Type': 'text/html'});
+      res.end("Settings Page");
+      break;
+
+
+
+    case "/socket":
+        res.writeHead(200,{'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*'});
+        res.end(`<html>
 <head>
 <script>
 window.onload = () => {
-  var ws = new WebSocket('ws://' + 192.168.1.106, 'protocolOne');
+  var ws = new WebSocket('ws://' + location.host, 'protocolOne');
   var btn = document.getElementById('btn');
   var led = document.getElementById('led');
   ws.onmessage = evt => {
@@ -68,41 +91,47 @@ window.onload = () => {
 </script>
 </head>
 <body>
-  <p>Button: <span id="btn">up</span></p>
+  <p>Led status: <span id="btn">up</span></p>
   <p>
-    LED on:
+    LED:
     <select id="led">
       <option>off</option><option>on</option>
     </select>
   </p>
 </body>
 </html>`);
-          break;
+    break;
 
-        default:
-          res.writeHead(404);
-          res.end("404");
-          break;
-      }
-
-    }).listen(80);
-
-    WebSocket.on("websocket", ws => {
-      console.log("new client");
-      clients.push(ws);
-
-      ws.on('message', msg => {
-        digitalWrite(buildInLed, msg == 'on');
-        console.log("msg: " + msg);
-      });
-
-      ws.on('close', evt => {
-        var x = clients.indexOf(ws);
-
-        if (x > -1) {
-          clients.splice(x, 1);
-        }
-      });
-    });
+    default:
+      res.writeHead(404);
+      res.end("404");
+      break;
   }
+}
+
+function broadcast(msg) {
+  clients.forEach(cl => cl.send(msg));
+}
+
+function startServer()
+{
+  const s = require('ws').createServer(serverHandler);
+  s.on('websocket', wsHandler);
+  s.listen(80);
+}
+
+
+wifi.connect(WIFI_NAME, WIFI_OPTIONS, err => {
+  if (err !== null) {
+    throw err;
+  }
+
+  wifi.getIP((err, info) => {
+    if (err !== null) {
+      throw err;
+    }else {
+      print("http://"+info.ip);
+      startServer();
+    }
+  });
 });
